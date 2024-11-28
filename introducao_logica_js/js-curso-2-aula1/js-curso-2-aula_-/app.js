4e2be806fd57817793809e8c9267e74ef9200849
romersonrsantana <romersonsantana@hotmail.com.br>
2024-11-28 20:21:50 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- introducao_logica_js/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ introducao_logica_js/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,1 +1,13 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Secret number game';
+
+let paragraphe = document.querySelector('p');
+paragraphe.innerHTML = 'Choose a number between 1 and 11...';
+
+function verificarChute(){
+    console.log('O botão foi clicado!')
+};
+
+
+
 // Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.
