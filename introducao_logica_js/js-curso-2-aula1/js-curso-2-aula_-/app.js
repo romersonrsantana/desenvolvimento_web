c627920244f7d668a32e3d8443340e533325d1ac
romersonrsantana <romersonsantana@hotmail.com.br>
2024-11-29 22:11:41 -0300

    de passo em passo
    Sagrado Coração de Jesus, Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja, Louvado. Amém.

--- introducao_logica_js/js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ introducao_logica_js/js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,12 +1,26 @@
+//Armazena o número secreto...
+let numeroSecrety = gerarNumberAleatorio();
+
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Secret number game';
 
 let paragraphe = document.querySelector('p');
 paragraphe.innerHTML = 'Choose a number between 1 and 11...';
 
+//Exibe na tela o número secreto
 function verificarChute(){
-    console.log('O botão foi clicado!')
+    //console.log(`Secret number is ${numeroSecrety} !!`)
+    let number = document.querySelector('input').value; //criar variável -> pegar o valor digitado no input
+    console.log(number == numeroSecrety);
 };
+
+//Para gerar um númber temos que usar 'return'
+function gerarNumberAleatorio() {
+    return parseInt(Math.random()*10 + 2);
+}
+
+//Funções com retorno, com parâmetros, sem retornos e semm parâmetros.
+
 
 
 
