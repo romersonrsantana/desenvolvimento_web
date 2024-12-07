37c3a07f05e7a8e1686de0a0d9d38d6892ac4003
romersonrsantana <romersonsantana@hotmail.com.br>
2024-12-06 22:17:57 -0300

    de passo em passo
    Sagrado Coração de Jesus, Confio e Espero Em Vós. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.

--- introducao_logica_js/desafio/des003/des003.js
+++ introducao_logica_js/desafio/des003/des003.js
@@ -1,10 +1,29 @@
 //Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel e Moisés E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
 
+let paragraphe = document.getElementById('mensage');
+
+function layout(Wher,msg){
+    Wher.innerHTML = msg;
+}
+
 function index(){
     let heightPerson = document.getElementById('id-altura').value;
     let weightPerson = document.getElementById('id-peso').value;
 
     let total = Number(weightPerson)/Number(heightPerson*heightPerson);
 
-    console.log(total)
+    if (total < 18.5){
+        //paragraphe.innerHTML = '>>> Thinness: Body mass index below ideal.';
+        layout(paragraphe, '>>> Thinness: Body mass index below ideal.');
+    } else if (total < 24.9){
+        layout(paragraphe, '>>> Normal');
+    } else if (total < 29.9){
+        layout(paragraphe, '>>> Sobrepeso');
+    } else if (total < 34.9){
+        layout(paragraphe, '>>> Obesidade grau I');
+    } else if (total < 39.9){
+        layout(paragraphe, '>>> Obesidade grau II');
+    } else{
+        layout(paragraphe, '>>> Obesidade grau III');
+    }
 }
