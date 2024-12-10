0d1caf11dabe9e589207f5df00379c31bed875f7
romersonrsantana <romersonsantana@hotmail.com.br>
2024-12-10 16:02:51 -0300

    de passo em passo
    Sagrado Coração de Jesus Confio e Espero em Vós. Louvado Seja Nosso Senhor Jesus Cristo, Para Sempre Seja Louvado. Amém.

--- introducao_logica_js/desafio/des004/des004.js
+++ introducao_logica_js/desafio/des004/des004.js
@@ -1,1 +1,26 @@
 //Toda Honra e Toda Glória Ao Deus de Abraão, Isaac, Jacó, Israel E Ao Seu Filho Amado Jesus Cristo. Louvado Seja Nosso Senhor Jesus Cristo. Para Sempre Seja Louvado. Amém.
+
+function calculeteFactorial(numero){
+    if (numero === 0 || numero === 1){
+        return 1;
+    }
+
+    let fatorial = 1;
+
+    for (let i = 2; i <= numero; i++){
+        fatorial *= i;
+    }
+
+    return fatorial;
+}
+
+//let num = document.getElementById('id-factorial').value;
+let num = 5;
+
+let total = calculeteFactorial(num);
+
+//let paragraphe = document.getElementById('mensage-id-p');
+
+console.log(total);
+
+//paragraphe.innerHTML = `The factorial number of ${numero} is ${total}`;
